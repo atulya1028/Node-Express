061be2ffb45ea56a307c4e50898597a128289e41
atulya1028 <atulyaagrawal1996@gmail.com>
2023-04-11 21:41:15 +0530

    Node Express

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id:1,
+        name: 'Atulya',
+        email: 'atulya@gmail.com'
+    },
+    {
+        id:2,
+        name: 'Vansh',
+        email: 'vansh@gmail.com'
+    },
+    {
+        id:3,
+        name: 'Raj',
+        email: 'raj@gmail.com'
+    },
+    {
+        id:4,
+        name: 'Rahul',
+        email: 'rahul@gmail.com'
+    },
+];
+
+module.exports = users;
